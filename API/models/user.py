9005909068f3fbf453c33f09b1aa6aaa69fd5e8e
nicomesa20 <nicomesa2013@gmail.com>
2020-07-05 22:12:31 -0500

    Added User to admin

--- API/models/user.py
+++ API/models/user.py
@@ -10,6 +10,6 @@ class User(models.Model):
     password = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.first_name
+        return '{} - {}: {}'.format(self.pk, self.first_name, self.email)
 
     
